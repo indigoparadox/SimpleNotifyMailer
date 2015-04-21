5bc1be2ae62bd4f776aea89cc2e07ea09cd9a5d0
indigoparadox <indigo.repo@interfinitdynamics.info>
2015-04-21 08:54:28 -0400

    Deleted a bunch of old junk code.

--- SimplePipes/SimplePipeClient.cs
+++ SimplePipes/SimplePipeClient.cs
@@ -10,45 +10,8 @@ using System.Text;
 namespace SimpleUtils {
     public class SimplePipeClient : SimplePipePeer {
 
-        public SimplePipeClient( string pipeNameIn ) : base( pipeNameIn ) { 
-
-#if false
-            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            Random random = new Random();
-            this.clientID = new string(
-                Enumerable.Repeat( chars, 8 )
-                    .Select(s => s[random.Next(s.Length)])
-                    .ToArray()
-            );
-#endif
+        public SimplePipeClient( string pipeNameIn ) : base( pipeNameIn ) {
 
         }
-
-#if false
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns>True if successfully connected, false otherwise.</returns>
-        public bool Connect() {
-            SafeFileHandle pipeHandle = CreateFile(
-                FormatPipeName(this.pipeName),
-                GENERIC_READ | GENERIC_WRITE,
-                0,
-                IntPtr.Zero,
-                OPEN_EXISTING,
-                FILE_FLAG_OVERLAPPED,
-                IntPtr.Zero
-            );
-
-            //could not get a handle to the named pipe
-            if( pipeHandle.IsInvalid ) {
-                return false;
-            }
-
-            this.pipeStream = new FileStream( pipeHandle, FileAccess.ReadWrite, (int)BUFFER_SIZE, true );
-
-            return true;
-        }
-#endif
     }
 }
--- SimplePipes/SimplePipeServer.cs
+++ SimplePipes/SimplePipeServer.cs
@@ -22,43 +22,5 @@ namespace SimpleUtils {
 
             this.client = false;
         }
-
-#if false
-        protected void ListenThread() {
-            while( this.active ) {
-                this.pipeServer.WaitForConnection();
-                try {
-                    while( true ) {// Loop until we get a disconnect exception or break.
-                        string readLine = this.pipeServerStream.ReadLine();
-                        if( readLine.StartsWith( "$SimplePipeCommand" ) ) {
-                            string[] readCommandArray = readLine.Split( '.' );
-                            if( readCommandArray[1].Equals( "NewPipe" ) ) {
-                                this.OpenPeerPipes( readCommandArray[2] );
-                                // TODO: Waiting here is a bad idea.
-                                this.serverPipeHandles[readCommandArray[2]].WaitForConnection();
-                                this.clientPipeHandles[readCommandArray[2]].Connect();
-                            }
-                        }
-                    }
-                } catch( IOException ) {
-                    // TODO
-                }
-            }
-        }
-
-#endif
-
-#if false
-        protected override void OpenPeerPipes( string clientCodeIn ) {
-            //Debug.Write( clientCodeIn );
-
-            this.serverPipeHandles.Add( clientCodeIn, SimplePipePeer.OpenReadServerPipe( SimplePipePeer.FormatPipeName( this.pipeName, false ) + "Server" + clientCodeIn ) );
-            this.serverPipeStreamHandles.Add( clientCodeIn, new StreamReader( this.serverPipeHandles[clientCodeIn] ) );
-
-            this.clientPipeHandles.Add( clientCodeIn, new NamedPipeClientStream( SimplePipePeer.FormatPipeName( this.pipeName, false ) + "Client" + clientCodeIn ) );
-            this.clientPipeStreamHandles.Add( clientCodeIn, new StreamWriter( this.clientPipeHandles[clientCodeIn] ) );
-        }
-#endif
-
     }
 }
